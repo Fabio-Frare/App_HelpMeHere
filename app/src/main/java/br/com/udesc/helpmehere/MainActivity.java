dbf4dd1b568c3a1314a3779b1afd7bc95be48782
Fabio <nabata99@gmail.com>
2021-06-27 17:25:11 -0300

    Commit configuração de câmera

--- app/src/main/java/br/com/udesc/helpmehere/MainActivity.java
+++ app/src/main/java/br/com/udesc/helpmehere/MainActivity.java
@@ -1,12 +1,20 @@
 package br.com.udesc.helpmehere;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     static ArrayList<String> arrayList = new ArrayList<>();
     MainAdapter adapter;
     Button btBombeiros;
+    ImageView imgFoto;
 
     public static void closeDrawer(DrawerLayout drawerLayout) {
         if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -35,11 +44,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Verificando se o usuário permitiu o uso da câmera
+        verificarPermissaoCamera();
+
         // Inicializando as variáveis
         drawerLayout = findViewById(R.id.drawer_layout);
         btMenu = findViewById(R.id.bt_menu);
         reciclerView = findViewById(R.id.recycler_view);
         btBombeiros = findViewById(R.id.btBombeiros);
+        imgFoto = findViewById(R.id.idFoto);
 
         // Limpando a lista
         arrayList.clear();
@@ -63,11 +76,42 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        btBombeiros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tirarFoto();
+            }
+        });
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         closeDrawer(drawerLayout);
+    }
+
+    // Verificando se o usuário permitiu o uso da câmera
+    private void verificarPermissaoCamera() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new  String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+
+    private void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    // Pegando o arquivo da foto
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap foto = (Bitmap) data.getExtras().get("data");
+            imgFoto.setImageBitmap(foto);
+        }
+
     }
 }
